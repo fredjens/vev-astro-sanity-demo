2de773f0c2276fd2b15868fc0ba299838833e1dc
Fredrik Jensen <post@fredrikjensen.com>
2024-10-04 09:10:39 +0200

    update vev script

--- install-vev.js
+++ install-vev.js
@@ -1,6 +1,8 @@
 import fs from "fs";
 
-async function downloadFiles() {
+const dist = "public/vev/";
+
+async function downloadVevFiles() {
   const url = "http://localhost:3000/api/project/6A5lyRJAZs?dev=true";
   const manifest = await fetch(url).then((res) => res.json());
   for (const file of manifest.files) {
@@ -8,11 +10,10 @@ async function downloadFiles() {
     const blob = await res.blob();
     const arrayBuffer = await blob.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
-    const dist = "public/vev/";
     const dir = dist + file.path.substring(0, file.path.lastIndexOf("/"));
     fs.mkdirSync(dir, { recursive: true });
     fs.writeFileSync(dist + file.path, buffer);
   }
 }
 
-downloadFiles();
+downloadVevFiles();
